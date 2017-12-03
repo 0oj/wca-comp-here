613d6e962bc4a1844b31fb972ea6b8265351c697
OojAmit <oojamit@gmail.com>
2017-12-03 17:03:27 +0530

    Added notiifications with links, an image and information about the comp.

--- index.js
+++ index.js
@@ -2,26 +2,54 @@ const fs = require('fs');
 const request = require('request');
 const path = require('path');
 const cheerio = require('cheerio');
+const notifier = require('node-notifier');
+const open = require('open');
 
 const url_noida = 'https://www.worldcubeassociation.org/competitions?utf8=%E2%9C%93&region=India&search=noida&state=present&year=all+years&from_date=&to_date=&delegate=&display=list';
 const url_delhi = 'https://www.worldcubeassociation.org/competitions?utf8=%E2%9C%93&region=India&search=delhi&state=present&year=all+years&from_date=&to_date=&delegate=&display=list';
-
+const url_india = 'https://www.worldcubeassociation.org/competitions?utf8=%E2%9C%93&region=India&search=&state=present&year=all+years&from_date=&to_date=&delegate=&display=list';
 
 request(url_noida, (err, res, body) => {
   var $ = cheerio.load(body);
-  var comps = '';
+  var comp_html = '';
+  var comp_array = [];
+
 
   $('li.not-past', 'div#upcoming-comps').each(function(){
-    comps += $(this).html();
+    comp_html += $(this).html();
+    comp_array.push($(this).html());
   })
 
-  var last = fs.readFileSync('./comps.html', 'utf8')
 
+  fs.readFile('./comps.html', 'utf8', (err, data) => {
 
-  if(last === comps){
-    console.log('It\'s the same thing after all. It\'s the same thing after all it the dame thing aaaaaaaaaaafter it the same damn thing!');
-  }else{
-    fs.writeFile('./comps.html', comps, err => {if(err){console.log(err)}})
-  }
+    if(data === comp_html){
+      console.log('It\'s the same thing after all. It\'s the same thing after all! It\'s the same thing aaaaAAAaaaaafter all. It is the same damn thing!');
+    }else{
+      var comps = comp_array.map(comp => {
+        comp = cheerio.load(comp)
+        return {
+          name: comp('span.competition-info p.competition-link a').text(),
+          date: comp('span.date').text().replace(/(\r\n|\n|\r|  )/gm,""),
+          location: comp('span.competition-info p.location').text().replace(/(\r\n|\n|\r|  )/gm,""),
+          venue: comp('span.competition-info div.venue-link p').text().replace(/(\r\n|\n|\r|  )/gm,""),
+          link: 'https://www.worldcubeassociation.org' + comp('span.competition-info p.competition-link a').attr('href')
+        }
+      })
+
+      comps.forEach(comp => {
+        notifier.notify({
+          title: comp.name,
+          message: `${comp.date} \n ${comp.venue} \n ${comp.location}`,
+          icon: path.join(__dirname, 'logo.jpg'), // Absolute path (doesn't work on balloons)
+          sound: true, // Only Notification Center or Windows Toasters
+          wait: true // Wait with callback, until user action is taken against notification
+        }, (err, response) => { if(err){console.error(err)}});
+        notifier.on('click', (notifierObject, options) => open(comp.link));
+      })
+
+      fs.writeFile('./comps.html', comp_html, err => {if(err){console.log(err)}})
+    }
+  })
 
 })
