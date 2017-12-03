fb13745c6fbfa0f8bcc9599175317b478d15d944
OojAmit <oojamit@gmail.com>
2017-12-03 11:57:36 +0530

    Check if HTML is the same, else write the new HTML

--- index.js
+++ index.js
@@ -3,11 +3,25 @@ const request = require('request');
 const path = require('path');
 const cheerio = require('cheerio');
 
-const url_noida = 'https://www.worldcubeassociation.org/competitions?region=India&search=Nodia&state=present&year=all+years&display=list';
-const url_delhi = 'https://www.worldcubeassociation.org/competitions?region=India&search=Delhi&state=present&year=all+years&display=list';
+const url_noida = 'https://www.worldcubeassociation.org/competitions?utf8=%E2%9C%93&region=India&search=noida&state=present&year=all+years&from_date=&to_date=&delegate=&display=list';
+const url_delhi = 'https://www.worldcubeassociation.org/competitions?utf8=%E2%9C%93&region=India&search=delhi&state=present&year=all+years&from_date=&to_date=&delegate=&display=list';
 
 
 request(url_noida, (err, res, body) => {
-  fs.WriteStream('./comps.html').write(body)
-  console.log('Were done!');
+  var $ = cheerio.load(body);
+  var comps = '';
+
+  $('li.not-past', 'div#upcoming-comps').each(function(){
+    comps += $(this).html();
+  })
+
+  var last = fs.readFileSync('./comps.html', 'utf8')
+
+
+  if(last === comps){
+    console.log('It\'s the same thing after all. It\'s the same thing after all it the dame thing aaaaaaaaaaafter it the same damn thing!');
+  }else{
+    fs.writeFile('./comps.html', comps, err => {if(err){console.log(err)}})
+  }
+
 })
